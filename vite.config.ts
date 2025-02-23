0b6e974021c47458e94680ec7830adf8ceecfbb2
Rohit <rohitkadav01@gmail.com>
2025-02-23 15:49:04 +0530

    Finalized

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Task-Landing_Page/",  // 👈 Important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
